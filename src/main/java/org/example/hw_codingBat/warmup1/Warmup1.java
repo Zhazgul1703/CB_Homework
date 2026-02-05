cf435f483c9d102a296394eab6da79f5d2e5b607
Zhazgul1703 <jazgul.tuleberdieva123@gmail.com>
2026-02-05 20:00:07 +0600

    Commit 7

--- src/main/java/org/example/hw_codingBat/warmup1/Warmup1.java
+++ src/main/java/org/example/hw_codingBat/warmup1/Warmup1.java
@@ -52,4 +52,14 @@ public class Warmup1 {
             return  false;
         }
     }
+
+    public boolean nearHundred(int n) {
+        if(Math.abs(100 - n)<= 10 ){
+            return true;
+        }else if(Math.abs(200 - n)<=10){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
--- src/test/java/hw_codingBat/Warmup1Test.java
+++ src/test/java/hw_codingBat/Warmup1Test.java
@@ -134,6 +134,47 @@ public class Warmup1Test extends BaseTest{
        Assertions.assertTrue(true);
        Assertions.assertTrue(true);
     }
+//    nearHundred(93) → true	true	OK
+//    nearHundred(90) → true	true	OK
+//    nearHundred(89) → false	false	OK
+//    nearHundred(110) → true	true	OK
+//    nearHundred(111) → false	false	OK
+//    nearHundred(121) → false	false	OK
+//    nearHundred(-101) → false	false	OK
+//    nearHundred(-209) → false	false	OK
+//    nearHundred(190) → true	true	OK
+//    nearHundred(209) → true	true	OK
+//    nearHundred(0) → false	false	OK
+//    nearHundred(5) → false	false	OK
+//    nearHundred(-50) → false	false	OK
+//    nearHundred(191) → true	true	OK
+//    nearHundred(189) → false	false	OK
+//    nearHundred(200) → true	true	OK
+//    nearHundred(210) → true	true	OK
+//    nearHundred(211) → false	false	OK
+//    nearHundred(290) → false	false	OK
+    @Test
+    void nearHundred(){
+        Assertions.assertTrue(true);
+        Assertions.assertTrue(true);
+        Assertions.assertFalse(false);
+        Assertions.assertTrue(true);
+        Assertions.assertFalse(false);
+        Assertions.assertFalse(false);
+        Assertions.assertFalse(false);
+        Assertions.assertFalse(false);
+        Assertions.assertTrue(true);
+        Assertions.assertTrue(true);
+        Assertions.assertFalse(false);
+        Assertions.assertFalse(false);
+        Assertions.assertFalse(false);
+        Assertions.assertTrue(true);
+        Assertions.assertFalse(false);
+        Assertions.assertTrue(true);
+        Assertions.assertTrue(true);
+        Assertions.assertFalse(false);
+        Assertions.assertFalse(false);
+    }
 
 
 }
