921f354c6d879da665f9a607986b2b69d5271f67
Zhazgul1703 <jazgul.tuleberdieva123@gmail.com>
2026-02-06 00:05:47 +0600

    fix: updated tests to check actual logic

--- src/main/java/org/example/hw_codingBat/warmup1/Warmup1.java
+++ src/main/java/org/example/hw_codingBat/warmup1/Warmup1.java
@@ -2,11 +2,7 @@ package org.example.hw_codingBat.warmup1;
 
 public class Warmup1 {
     public boolean sleepIn(boolean weekday, boolean vacation) {
-        if (!weekday || vacation){
-            return true;
-        }else {
-            return false;
-        }
+        return(!weekday || vacation);
     }
 
     public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
